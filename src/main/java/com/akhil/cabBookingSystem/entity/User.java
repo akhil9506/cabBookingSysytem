7af2e995f03835dc1adaaa61c966faf1d6d559f7
akhil <f20180458@pilani.bits-pilani.ac.in>
2021-12-08 14:31:48 +0530

    database migrated to postgresql

--- src/main/java/com/akhil/cabBookingSystem/entity/User.java
+++ src/main/java/com/akhil/cabBookingSystem/entity/User.java
@@ -1,11 +1,11 @@
 package com.akhil.cabBookingSystem.entity;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+
+
+import javax.persistence.*;
 
 @Entity
+@Table(name="Credentials")
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
